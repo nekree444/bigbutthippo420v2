dd1ad2f9a9affb262a92853d40a2f78187a946f6
nekree444 <nekrre@gmail.com>
2024-07-04 10:42:58 -0400

    added redirects + testing bump version

--- server.py
+++ server.py
@@ -30,5 +30,26 @@ def original():
         'index_1.html'
     )
 
+@app.route('/instagram')
+def instagram():
+    return render_template(
+        'redirects.html',
+        link='https://instagram.com/thomasm.444'
+    )
+
+@app.route('/snap')
+def instagram():
+    return render_template(
+        'redirects.html',
+        link='https://snapchat.com/add/thomasm444'
+    )
+
+@app.route('/discord')
+def instagram():
+    return render_template(
+        'redirects.html',
+        link='https://discord.gg/UKX6GwX2x6'
+    )
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
