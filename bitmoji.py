d4dbb105cc25b47701182361622eef6531f9dd60
nekree444 <nekrre@gmail.com>
2024-07-10 04:05:09 -0400

    added wheelchair compatibility bump version

--- bitmoji.py
+++ bitmoji.py
@@ -6,7 +6,7 @@ def bitmoji_webp(snap='thomasm444'):
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
 
     r = requests.get(f'https://www.snapchat.com/add/{snap}', headers=headers)
-    return parser.HTMLParser(r.text).css_first('picture.Bitmoji3DImage_webPImage__sMFoZ').css_first('source').attributes.get('srcset')
+    return parser.HTMLParser(r.text).css_first('picture.Bitmoji3DImage_webPImage__sMFoZ').css_first('source').attributes.get('srcset').replace('-wc-', '-')
 
 if __name__ == '__main__':
     print('\n--- 😅😅😅😅😅😅😅😅😅😅 ---\n')
